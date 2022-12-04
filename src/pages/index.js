fc520d7bfec8771facbe61d2d3202f8d9e749b5c
2312323123 <2312323@protonmail.com>
2022-12-04 09:54:53 +0100

    icon visible now, probably

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,10 +95,10 @@ export default ({ location }) => {
 
 export const Head = () => (
   <>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <title>BITEhack 2023</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/x-icon" href="../images/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../favicon.ico" />
     <meta
       name="description"
       content="BITEhack jest konkursem programistyczno- robotycznym dla studentów organizowanym w Klubie Studio zaraz przy Akademii Górniczo-Hutniczej w Krakowie."
